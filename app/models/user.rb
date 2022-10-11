479b151bda64946f2edfa78bed3620bd985af7c8
BHDHakim <hakim.bhd@gmail.com>
2022-10-11 22:27:52 +0100

    Configure Devise gem

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable, :confirmable
+
   has_many :deals
   has_many :groups
 
