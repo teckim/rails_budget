e889be2fac3866b664f280a629240bae98f4ffc1
BHDHakim <hakim.bhd@gmail.com>
2022-10-13 19:29:03 +0100

    Fix user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
 
-  has_many :deals, dependent: :destroy
+  has_many :deals, foreign_key: 'author_id', dependent: :destroy
   has_many :groups, dependent: :destroy
 
   validates :name, presence: true, length: { in: 2..25 }
