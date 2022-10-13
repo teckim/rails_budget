576e71016a6b48b715334639bae6e343182cd397
BHDHakim <hakim.bhd@gmail.com>
2022-10-13 14:59:25 +0100

    Update deal and group models

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,6 +1,11 @@
 class Group < ApplicationRecord
   belongs_to :user
-  has_and_belongs_to_many :deal
+  has_and_belongs_to_many :deals
 
   validates :name, presence: true, length: { in: 3..50 }
+  validates :icon, presence: true
+
+  def total_amount
+    deals.sum('amount')
+  end
 end
