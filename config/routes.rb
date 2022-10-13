b697db5056ebe611eee8f7be1a6ce153525b6e46
BHDHakim <hakim.bhd@gmail.com>
2022-10-13 15:03:09 +0100

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,12 @@ Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  resources :groups, except: [:update] do
+    resources :deals
+  end
+
+  resources :deals, only: %i[new create]
+
   # Defines the root path route ("/")
-  root "home#index"
+  root 'home#index'
 end
